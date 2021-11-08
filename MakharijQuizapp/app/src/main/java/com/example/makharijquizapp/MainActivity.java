f5892c4c05286df94e8e184f961a288cd9bb1a81
Hamza Khan <74649522+HamzaaaKhan@users.noreply.github.com>
2021-11-08 23:48:37 +0500

    Constraint layouts and button to repository is added

--- MakharijQuizapp/app/src/main/java/com/example/makharijquizapp/MainActivity.java
+++ MakharijQuizapp/app/src/main/java/com/example/makharijquizapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.makharijquizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Repository(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/HamzaaaKhan/Assignment-02-BITF18A535/tree/master/MakharijQuizapp"));
+        startActivity(browserIntent);
     }
 }
